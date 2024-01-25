2a943f4a2d380e91ad79bd6b86e85fcf774a5d49
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-25 16:22:31 +0800

    Displayed the output of both column and row

--- asterisk_inverted_pyradamid.py
+++ asterisk_inverted_pyradamid.py
@@ -13,5 +13,7 @@ for column in range(5):
 #Create a loop for the row pattern
     for row in range(column + 1, 5):
 #Print the output of row through asterisks
+        print("*", end = " ")
 #Print the output of column through asterisks
+    print("*")
 
