5d809843919132efce9c2b23ebc22809a69b3116
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-25 16:16:37 +0800

    Created the for loops for both row and column

--- asterisk_inverted_pyradamid.py
+++ asterisk_inverted_pyradamid.py
@@ -9,7 +9,9 @@
 # *
 
 #Create a loop for the column pattern
+for column in range(5):
 #Create a loop for the row pattern
+    for row in range(column + 1, 5):
 #Print the output of row through asterisks
 #Print the output of column through asterisks
 
