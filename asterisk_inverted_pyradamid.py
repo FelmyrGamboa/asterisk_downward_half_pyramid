fcaf88585c4b47a634dd9e64056596ba7a57c1fb
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-25 16:14:32 +0800

    Pseudocodes

--- asterisk_inverted_pyradamid.py
+++ asterisk_inverted_pyradamid.py
@@ -1,2 +1,15 @@
 # Exercise 14: Print a downward Half-Pyramid Pattern of Star (asterisk)
 
+# Expected Output
+
+# * * * * *  
+# * * * *  
+# * * *  
+# * *  
+# *
+
+#Create a loop for the column pattern
+#Create a loop for the row pattern
+#Print the output of row through asterisks
+#Print the output of column through asterisks
+
